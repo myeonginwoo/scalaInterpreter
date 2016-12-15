ceff284608dd38af7d3523537f22ac6723270bd4
Myeongin Woo <myeonginwoo@gmail.com>
2016-12-15 11:41:28 +0900

    create project

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "ScalaInterpreter"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by Lazysoul on 2016. 12. 15..
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("test")
+  }
+}
